c98b31bf253a8f80ded858cba1f08e11f568b5b4
Hans de Rooij <0912423@hr.nl>
2017-03-24 15:40:46 +0100

    Started with the client-side API.
    Dikke stapel api methods geschreven. maar nog niet klaar.
    nu mijn ouders maar eens opzoeken. In de trein verder typen

---
+++ java/ecorp/client/src/main/java/JsonResponse.java
@@ -0,0 +1,13 @@
+import com.google.gson.JsonElement;
+
+/**
+ * Created by Hans de Rooij on 24/03/2017.
+ */
+public class JsonResponse {
+    public JsonResponse(String type, String IBAN) {
+        this.IBAN = IBAN;
+        this.type = type;
+    }
+    public String type;
+    public String IBAN;
+}
