6f1917f0d511cbbdc9ca6297fadeee0139095a16
Hans de Rooij <0912423@hr.nl>
2017-03-24 13:37:43 +0100

    Adds maven dependency GSON to server and client

--- java/ecorp/client/src/main/java/API.java
+++ java/ecorp/client/src/main/java/API.java
@@ -1,8 +1,13 @@
+import com.google.gson.*;
+
 /**
  * Created by Hans de Rooij on 24/03/2017.
  */
 public class API {
     public static boolean isCorrectCard(String accountNumber, String passNumber) {
-        
+        Gson gson = new Gson();
+        String json = gson.toJson(new Object(){private String hoi = "hoi";});
+        System.out.println(json);
+        return true;
     }
 }
