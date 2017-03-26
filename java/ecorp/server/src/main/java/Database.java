27ee3a84119d2315c8cde91d97562881d63b51cf
Noëlle <Noelle@mbp-van-noelle.home>
2017-03-26 23:22:43 +0200

    begonnen met de methodes voor dingen opvragen uit database

--- java/ecorp/server/src/main/java/Database.java
+++ java/ecorp/server/src/main/java/Database.java
@@ -58,8 +58,13 @@ public class Database {
         } catch (SQLException e) {
             logger.error("Execution of query select saldo failed", e); //moet nog wat uitgebreider
         }
-        return 0;
+        return saldo;
     }
+
+
+
+
+
 
     public boolean withDraw(String rekeningNr, int amount) {
         //TODO nu (22-3) staat saldo nog als int in database
@@ -98,12 +103,90 @@ public class Database {
         return false;
     }
 
-    //TODO rekeningnr info opvragen (of het in de database staat)
-    //TODO pasnr info opvragen (of het in de database staat)
-    //TODO pincode opvragen
-    //TODO pincode foutief veranderen (verhogen of weg)
-    //TODO pincode foutief opvragen
-    //TODO iets met daglimiet
+
+    //TODO kijken of deze compareRekeningNr methode beetje sense maakt
+    public boolean compareRekeningNr(String rekeningNr) {
+        //TODO varchar in sql goed als string in java?
+
+
+        try{
+            PreparedStatement ps = con.prepareCall("SELECT rekeningNr"
+                                                    + "FROM E-corp.klant"
+                                                    + "WHERE E-corp.klant.rekeningNr = ?");
+            ps.setString(1,rekeningNr);
+            rs = ps.executeQuery();
+            if (rs.next()){
+                return true;
+            }
+
+            logger.debug("rekeningNr bestaat niet");
+            return false;
+        }
+        catch (SQLException e){
+            logger.error("Execution of query select rekeningNr failed", e);
+        }
+
+        return false;
+    }
+
+    //TODO kijken of deze comparePasNr methode beetje sense maakt
+    public boolean comparePasNr (int pasNr) {
+        //TODO int in java goed als ? in sql?
+        //TODO letop: nog geen pas tabel
+
+
+        try{
+            PreparedStatement ps = con.prepareCall("SELECT pasNr"
+                                                    + "FROM E-corp.pas"
+                                                    + "WHERE E-corp.pas.pasNr = ?");
+            ps.setInt(1, pasNr);
+            rs = ps.executeQuery();
+            if (rs.next()){
+                return true;
+            }
+
+            logger.debug("pasNr bestaat niet");
+            return false;
+        }
+        catch (SQLException e){
+            logger.error("Execution of query select pasNr failed", e);
+        }
+
+        return false;
+    }
+
+    public int getPincode (int pasNr){
+        //TODO letop nog geen pas tabel in database
+        //TODO iets doen met dat 't max 4 cijfers mogen zijn of moet dat in API?
+        //TODO moet er nog niets qua veiligheid dat je niet zomaar pincode ertussenuit kunt opvragen?
+
+        int pincode = 0000;
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT pincode "
+                                                        + "FROM E-corp.pas "
+                                                        + "WHERE E-corp.pas.pasNr = ?");
+            ps.setString(1, pasNr);
+            rs = ps.executeQuery();
+            rs.next();
+            pincode = rs.getInt("saldo");
+            logger.debug("Saldo = " + pincode); //als handig kan ook pas/nr etc toegevoegd worden
+        } catch (SQLException e) {
+            logger.error("Execution of query getPincode failed", e); //moet nog wat uitgebreider
+        }
+        return pincode;
+    }
+
+
+
+
+    // rekeningnr info opvragen {check of rekeningnr in database} http://stackoverflow.com/questions/11288557/how-do-i-tell-if-a-row-exists-in-a-table
+    // pasnr info opvragen {check of pasnr in database } http://stackoverflow.com/questions/11288557/how-do-i-tell-if-a-row-exists-in-a-table
+    // pincode opvragen { get pincode where pasnr}
+    //TODO pincode foutief verhogen {set pincode_fout oid +1 }
+    //TODO pincode foutief weer nul {set pincode_fout oid 0}
+    //TODO pincode foutief opvragen {get pincode_fout}
+    //TODO iets met daglimiet (daglimiet in rekeningnr?) {get daglimiet where rekeningnr oid?}
 
     //TODO via command line de tabellen maken van database
+    //TODO elke try meer exception catches speciaal voor die try
 }
