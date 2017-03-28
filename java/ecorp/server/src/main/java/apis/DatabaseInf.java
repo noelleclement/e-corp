e68bb2c82c8c44d6f6865462bb3e4781244cb76c
Hans de Rooij <0912423@hr.nl>
2017-03-28 17:37:32 +0200

    Interface database changes

--- java/ecorp/server/src/main/java/apis/DatabaseInf.java
+++ java/ecorp/server/src/main/java/apis/DatabaseInf.java
@@ -3,6 +3,9 @@ package apis;
 public interface DatabaseInf{
 
     int getBalance(String rekeningNr);
-    boolean getWithdraw(String rekeningNr);
+    boolean getWithdraw(String rekeningNr, int amount);
+    boolean comparePasRekening(String rekeningNr, String PasNr);
+    boolean comparePincode(String PasNr, String pinCode);
+    boolean withdrawPossible(String rekeningNr, int amount);
 
 }
