d8200e7c0c8c446b7c85d4e883ea6aff400c5349
Noëlle <Noelle@145-24-242-178.wlan.hro.nl>
2017-03-31 14:32:08 +0200

    printcode testen

---
+++ java/ecorp/client/src/main/java/AppPrinter.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Noelle on 31-03-17.
+ */
+public class AppPrinter {
+
+
+    public static void main(String[] args){
+        Printer printer = new Printer(50);
+        printer.print(true);
+    }
+}
